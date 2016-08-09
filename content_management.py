b28d6bb746b77090eb938444fa0e841bf3bb3811
rahulgitty <rahul.k@8kmiles.com>
2016-08-09 14:48:53 +0530

    First Commit
    Added python files in First commit

---
+++ content_management.py
@@ -0,0 +1,10 @@
+def Content():
+	TOPIC_DICT = {"Brand Factory":[["Allen Solley","/allen-solly/"],
+									["Peter England","/peter-england/"],
+									["Raymond","/raymond/"]],
+
+				  "Special Store":[["Watches","/watches/"],
+				  				    ["Kitchen","/kitchen/"],
+				  				    ["Electronics","/electronics/"]]}
+
+	return TOPIC_DICT			  				    
